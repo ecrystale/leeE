5d84cff8139db04a15f1835be1f553d33f3707f7
ecrystale <elee19@stuy,edu>
2018-10-01 19:32:10 -0400

    fixed

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,28 +1,53 @@
+#Cathy Cai and Emily Lee - CaiLee Cosmetics
+#SoftDev1 pd6
+#K14 -- Do I know You?
+#2018-10-02
 
-from flask import Flask #, render_template
-#from util import
-
-app=Flask(__name__)
 from flask import Flask, session, render_template, url_for, redirect, request
 import os
 
 app=Flask(__name__)
 app.secret_key=os.urandom(32)
 
+
 @app.route("/")
 def login():
-    return render_template("/practice.html")
+    #add session
+    session["watermelon"]="juice"
+    return render_template("/login.html")
+
+
 
 @app.route("/welcome", methods=["POST"])
 def auth():
-    session["watermelon"]="juice"
+    
+    #wrong username/pass
     if request.form["user"]!="watermelon":
-        return render_template("/practice.html", error="Wrong Username")
+        return error('username or password')  
     if request.form["pass"]!=session["watermelon"]:
-        return render_template("/practice.html", error="Wrong Password")
-    else:
-        return render_template("/welcome.html",user="watermelon")
-        
+        return error('password')
+    
+    #right username and pass, go to welcome page!
+    if request.form["pass"]==session["watermelon"]:
+        return render_template("/welcome.html",
+                               user="Watermelon"
+                               )
+
+
+#wrong credentials, try again!
+def error(error):
+    return render_template("/login.html",
+                           error=error
+                           )
+
+
+@app.route("/logout")
+#logging out removes the user from the current session
+def logout():
+    session.pop("watermelon")
+    return redirect(url_for("login"))
+
+
 if __name__=="__main__":
     app.debug=True
     app.run()
